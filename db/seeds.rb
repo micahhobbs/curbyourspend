265a04bcf85b71aa4697659e8274a77aa5ef3818
Lachlan Munro <lachlangmunro@gmail.com>
2022-07-05 21:21:01 +1000

    Create starter seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,63 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts "Cleaning data..."
+Comment.destroy_all
+Item.destroy_all
+User.destroy_all
+puts "Finished cleaning data."
+puts "Creating data..."
+
+micah = User.create!(
+  email: "micah@email.com",
+  password: "password",
+  password_confirmation: "password"
+)
+
+phil = User.create!(
+  email: "phil@email.com",
+  password: "password",
+  password_confirmation: "password",
+)
+
+lachy = User.create!(
+  email: "lachy@email.com",
+  password: "password",
+  password_confirmation: "password",
+)
+
+
+item1 = Item.create!(
+  name: "Samsung 75 inch 4K Smart TV",
+  value: 2495,
+  description: "Extraordinary colour and brightness can be yours, with Quantum Dot technology on Q60B QLED TV. The AirSlim Design offers a sleek, minimalist profile, and with Smart Connectivity, controlling and connecting entertainment and devices can be quick and seamless. Long-lasting brilliance, colourful possibilities.",
+  link: "https://www.jbhifi.com.au/products/samsung-q60b-75-qled-4k-smart-tv-2022?view=tabs",
+  start_date: Date.new(2022, 7, 4),
+  end_date: Date.new(2022, 7, 14),
+  user_id: 1,
+)
+
+item2 = Item.create!(
+  name: "Samsung 75 inch 4K Smart TV",
+  value: 2495,
+  description: "Extraordinary colour and brightness can be yours, with Quantum Dot technology on Q60B QLED TV. The AirSlim Design offers a sleek, minimalist profile, and with Smart Connectivity, controlling and connecting entertainment and devices can be quick and seamless. Long-lasting brilliance, colourful possibilities.",
+  link: "https://www.jbhifi.com.au/products/samsung-q60b-75-qled-4k-smart-tv-2022?view=tabs",
+  start_date: Date.new(2022, 7, 4),
+  end_date: Date.new(2022, 7, 14),
+  user_id: 2,
+)
+
+item3 = Item.create!(
+  name: "Samsung 75 inch 4K Smart TV",
+  value: 2495,
+  description: "Extraordinary colour and brightness can be yours, with Quantum Dot technology on Q60B QLED TV. The AirSlim Design offers a sleek, minimalist profile, and with Smart Connectivity, controlling and connecting entertainment and devices can be quick and seamless. Long-lasting brilliance, colourful possibilities.",
+  link: "https://www.jbhifi.com.au/products/samsung-q60b-75-qled-4k-smart-tv-2022?view=tabs",
+  start_date: Date.new(2022, 7, 4),
+  end_date: Date.new(2022, 7, 14),
+  user_id: 3,
+)
+
+
+puts "Seed complete."
