b6e35bae36f0a228ff8eaef9200752a9c8c32fc1
Lachlan Munro <lachlangmunro@gmail.com>
2022-07-07 18:51:30 +1000

    Started on items controller with default actions.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,44 @@
 class ItemsController < ApplicationController
+
+  def index
+    @items = Item.all
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.user = current_user
+    if @item.save
+      redirect_to items_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+
+    redirect_to item_path(@item)
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+
+    redirect_to items_path
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :value, :description, :link, :start_date, :end_date)
+  end
 end
