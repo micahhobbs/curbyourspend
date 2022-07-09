3dcf03128236310a776c3dc0f4d42f79d7bac72d
Lachlan Munro <lachlangmunro@gmail.com>
2022-07-09 11:18:04 +1000

    Added image functionality.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :items
   has_many :comments
-
+  has_one_attached :photo
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
