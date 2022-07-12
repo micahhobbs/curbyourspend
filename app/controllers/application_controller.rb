20ea5828c9e380c6c27df37c4060953f491ff6dc
Lachlan Munro <lachlangmunro@gmail.com>
2022-07-12 20:22:25 +1000

    Users can now add profile pics. Cloudinary crops to users face.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :username, :profile_visible])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :email, :username, :profile_visible])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :username, :profile_visible, :avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :email, :username, :profile_visible, :avatar])
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,6 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:first_name, :last_name)
+    params.require(:user).permit(:first_name, :last_name, :avatar)
   end
 end
