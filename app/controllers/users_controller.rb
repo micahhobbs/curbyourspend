9beab4c1a98f5bd904d1420290bd4bf008295cdf
Lachlan Munro <lachlangmunro@gmail.com>
2022-07-14 21:57:10 +1000

    Added comment functionality. Need to incorporate as partial in future if time.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
   end
 
   private
+
   def user_params
     params.require(:user).permit(:first_name, :last_name, :avatar)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :items
+  resources :items do
+    resources :comments, only: [:new, :create]
+  end
+  resources :comments, only: [:destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/users', to: 'users#index'
   get '/users/:id', to: 'users#show', as: 'user_profile'
