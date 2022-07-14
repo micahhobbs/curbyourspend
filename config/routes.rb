00e31bfeb991cbc37049e473f48e47fae8491d83
Lachlan Munro <lachlangmunro@gmail.com>
2022-07-14 21:57:10 +1000

    Added comment functionality. Need to incorporate as partial in future if time.

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :items
+  resources :items do
+    resources :comments, only: [:new, :create]
+  end
+  resources :comments, only: [:destroy]
   resources :profiles, only: [:index, :show]
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/users', to: 'users#index'
+  get '/users/:id', to: 'users#show', as: 'user_profile'
   get "calendar", to: 'pages#calendar'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
