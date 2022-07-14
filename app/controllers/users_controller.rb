278d3ac626f981436dce05f702655bca8bb93827
Micah Hobbs <micahthobbs@gmail.com>
2022-07-14 19:03:55 +1000

    Profiles index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,9 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
+    @users = User.where(profile_visible: "true")
   end
 
   def show
     @user = User.find(params[:id])
-  end
-
-  private
-  def user_params
-    params.require(:user).permit(:first_name, :last_name, :avatar)
   end
 end
