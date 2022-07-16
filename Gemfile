e24074a106894f369f6e063f118f8ec5fb5aa904
Lachlan Munro <lachlangmunro@gmail.com>
2022-07-16 14:47:28 +1000

    Built main pieces of logic for screen and rough structure. Need to adjust charts and style.

--- Gemfile
+++ Gemfile
@@ -28,9 +28,9 @@ gem 'cloudinary', '~> 1.16.0'
 gem 'bootsnap', '>= 1.4.4', require: false
 
 gem 'devise'
-
+gem "groupdate"
 gem "simple_calendar", "~> 2.4"
-
+gem "chartkick"
 gem 'autoprefixer-rails', '10.2.5'
 gem 'font-awesome-sass', '~> 6.1', '>= 6.1.1'
 gem 'simple_form', github: 'heartcombo/simple_form'
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   end
   resources :comments, only: [:destroy]
   resources :profiles, only: [:index, :show]
+  get 'dashboard', to: 'pages#dashboard'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/users', to: 'users#index'
   get '/users/:id', to: 'users#show', as: 'user_profile'
