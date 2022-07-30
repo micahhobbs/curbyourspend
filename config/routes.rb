fb15519fdefa81b8f58aabc160ad83ca24362aad
Micah Hobbs <micahthobbs@gmail.com>
2022-07-30 15:47:25 +1000

    added some styled error pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'errors/not_found'
+  get 'errors/internal_server_error'
   devise_for :users
   root to: 'pages#home'
 
@@ -14,6 +16,9 @@ Rails.application.routes.draw do
   resources :profiles, only: [:index, :show]
 
   get 'dashboard', to: 'pages#dashboard'
+
+  match '/404', to: 'errors#not_found', via: :all
+  match '/500', to: 'errors#internal_server_error', via: :all
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/users', to: 'users#index'
